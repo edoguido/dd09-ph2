c479f455a7e1b5624371f03b70d7b73aacc750ef
lostphasma <alabardadellanebulosa@gmail.com>
2018-11-28 21:18:32 +0100

    Viz08 Lea Completa

--- _site/assets/js/viz08.js
+++ _site/assets/js/viz08.js
@@ -1,115 +1,84 @@
-var buttons = document.querySelectorAll('.button');
-var onlineText = document.getElementById("online-text");
-var nodataText = document.getElementById("nodata-text");
-var shutteddownText = document.getElementById("shutteddown-text");
+// var buttons = document.querySelectorAll('.button');
+// var onlineText = document.getElementById("online-text");
 
-var onlineGraph = document.getElementById("online");
-var nodataGraph = document.getElementById("nodata");
-var shutteddownGraph = document.getElementById("shutteddown");
+// // Onload
+// window.onload = onlineGraph.classList.add("on"),
+//                 onlineText.classList.add("white"),
+//                 onlineRect.classList.add("filled"),
+//                 console.log("caricato");
 
-var onlineRect = document.getElementById("online-box")
-var nodataRect = document.getElementById("nodata-box")
-var shutteddownRect = document.getElementById("shutteddown-box")
+// // Aggiungo event listeners
+// for (i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener('click', highlightButton, false);
+// }
 
-var divider = document.getElementById("rect-divider");
 
-// Onload
-window.onload = onlineGraph.classList.add("on"),
-                onlineText.classList.add("white"),
-                onlineRect.classList.add("filled"),
-                console.log("caricato");
+//------
+//------ LISTENERS
+//------ a tutti i .circles (sono gruppi che contengono tutti i cerchi)
+var circles = document.querySelectorAll('.circles');
+var arrows = document.querySelectorAll('.arrows');
+var toggle = document.getElementById('toggle');
 
-// Aggiungo event listeners
-for (i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', highlightButton, false);
+for (i = 0; i < circles.length; i++) {
+    circles[i].addEventListener('mouseover', highlightOn, false);
+    circles[i].addEventListener('mouseout', highlightOff, false);
 }
 
+//------
+//------ FUNCTIONS HIGHLIGHTACOS
+//------
+function highlightOn() {
+    var circleToIgnore;
 
-// Funzioni
-function highlightButton() {
-    console.log("click");
-    onlineGraph.classList.remove("on");
-    nodataGraph.classList.remove("on");
-    shutteddownGraph.classList.remove("on");
-
-    onlineRect.classList.remove("filled");
-    nodataRect.classList.remove("filled");
-    shutteddownRect.classList.remove("filled");
-    
-    onlineText.classList.remove("white");
-    nodataText.classList.remove("white");
-    shutteddownText.classList.remove("white");
-
-    if (this.classList.contains('online')) {
-        onlineGraph.classList.add("on");
-        onlineRect.classList.add("filled");
-        divider.style.fill = "#0000FF";
-        onlineText.classList.add("white");
+    //------controlla se tutti gli altri dots hanno la categoria del dot sul quale si trova il mouse
+    for (t = 0; t < circles.length; t++) {
+        //------controlla su quale pallino è hover
+        if (circles[t].matches('.circles:hover')) {
+            circleToIgnore = t;
+            console.log(t + " tacos")
+            arrows[t].classList.add("stroke-on");
+        } else if (t != circleToIgnore) {
+            circles[t].classList.add("off");
+            arrows[t].classList.add("off");
+        }
     }
+}
 
-    if (this.classList.contains('nodata')) {
-        nodataGraph.classList.add("on");
-        nodataRect.classList.add("filled");
-        divider.style.fill = "#7f7fff";
-        nodataText.classList.add("white");
+function highlightOff() {
+    for (i = 0; i < circles.length; i++) {
+        circles[i].classList.remove("off");
+        arrows[i].classList.remove("off");
+        arrows[i].classList.remove("stroke-on");
     }
+}
 
-    if (this.classList.contains('shutteddown')) {
-        shutteddownGraph.classList.add("on");
-        shutteddownRect.classList.add("filled");
-        divider.style.fill = "CE3B19";
-        shutteddownText.classList.add("white");
-    }
-}   
-
-/* ------------------------------------------- */
-/* -----------       TOGGLES       ----------- */
-/* ------------------------------------------- */
+//------
+//------ TOGGLE
+//------
 
 $(document).ready(function(){
-    $("#direct").click(function(){
-        $(".direct").toggleClass("direct-on");
-        $(".direct-numbers").toggleClass("numbers-on");
-        $(".direct-box").toggleClass("box-on");
+    $("#toggle-button").mouseenter(function(){
+        $(".hl-red").toggleClass("toggle-on"),
+        $(".fade").toggleClass("faded");
+        $(".toggle-box").toggleClass("box-on");
     });
 });
 
 $(document).ready(function(){
-    $("#referrals").click(function(){
-        $(".referrals").toggleClass("referrals-on");
-        $(".referrals-numbers").toggleClass("numbers-on");
-        $(".referrals-box").toggleClass("box-on");
+    $("#toggle-button").mouseleave(function(){
+        $(".hl-red").toggleClass("toggle-on"),
+        $(".fade").toggleClass("faded");
     });
 });
 
-$(document).ready(function(){
-    $("#search").click(function(){
-        $(".search").toggleClass("search-on");
-        $(".search-numbers").toggleClass("numbers-on");
-        $(".search-box").toggleClass("box-on");
-    });
-});
-
-$(document).ready(function(){
-    $("#social").click(function(){
-        $(".social").toggleClass("social-on");
-        $(".social-numbers").toggleClass("numbers-on");
-        $(".social-box").toggleClass("box-on");
-    });
-});
-
-$(document).ready(function(){
-    $("#mail").click(function(){
-        $(".mail").toggleClass("mail-on");
-        $(".mail-numbers").toggleClass("numbers-on");
-        $(".mail-box").toggleClass("box-on");
-    });
-});
-
-$(document).ready(function(){
-    $("#display").click(function(){
-        $(".display").toggleClass("display-on");
-        $(".display-numbers").toggleClass("numbers-on");
-        $(".display-box").toggleClass("box-on");
-    });
-});
+// $(function(){
+//     $('#toggle-button').hover(function() {
+//         console.log("fired"),
+//       $('.hl-red').removeClass('toggle-on'),
+//       $('.toggle-box').addClass('toggle-box-on');
+//     }, function() {
+//         console.log("watered"),
+//       $('.hl-red').addClass('toggle-on');
+//     })
+//   })
