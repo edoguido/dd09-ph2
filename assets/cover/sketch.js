66e4a33b92bec02c27723ee03fe2827f075fdfb8
Edoardo Guido <edoardo.guido.93@gmail.com>
2018-12-02 20:25:22 +0100

    Fixed texts in viz10, added p5 sketch as cover

--- assets/cover/sketch.js
+++ assets/cover/sketch.js
@@ -1,8 +1,9 @@
 var t;
-var colors= ['#8FA5FF','#150FAA','#0033FF','#DAA1FF','#650499','#CE3B19','#F78028','#F2C005'];
+var colors = ['#8FA5FF', '#150FAA', '#0033FF', '#DAA1FF', '#650499', '#CE3B19', '#F78028', '#F2C005'];
 var giroCompleto = 360;
 var mic;
 var mappedVolume;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
@@ -10,71 +11,69 @@ function setup() {
   noFill();
   t = 0;
 
-  mic= new p5.AudioIn();
+  mic = new p5.AudioIn();
+
   //facciamolo partire sto audio input
   mic.start();
-
 }
 
 var contatore = 0;
 
 function draw() {
-  var volume= mic.getLevel();
-  console.log(volume);
-  mappedVolume = map(volume,0,0.15,1,7);
-  console.log(mappedVolume);
+  var volume = mic.getLevel();
+  // console.log(volume);
+  mappedVolume = map(volume, 0, 0.0075, 1, 7);
+  // console.log(mappedVolume);
 
 
-//background(255);
+  //background(255);
 
-  translate(width/2, height/2);
+  translate(width / 2, height / 2);
   var colorProgress = frameCount % giroCompleto;
 
-  var mappedColor = map(colorProgress,0, giroCompleto - 1 ,0, 1);
+  var mappedColor = map(colorProgress, 0, giroCompleto - 1, 0, 1);
   //console.log(mappedColor);
   //console.log(contatore);
-// if (contatore==1){
-//   stroke('GREEN');
-// }
+  // if (contatore==1){
+  //   stroke('GREEN');
+  // }
 
-
-
-switch( contatore){
-  case 0:
-  stroke (lerpColor(color(colors[contatore]), color(colors[contatore+1]), mappedColor));
-  break;
-  case 1:
-  stroke (lerpColor(color(colors[contatore]), color(colors[contatore+1]), mappedColor));
-  break;
-  case 2:
-  stroke (lerpColor(color(colors[contatore]), color(colors[contatore+1]), mappedColor));
-  break;
-  case 3:
-  stroke (lerpColor(color(colors[contatore]), color(colors[contatore+1]), mappedColor));
-  break;
-  case 4:
-  stroke (lerpColor(color(colors[contatore]), color(colors[contatore+1]), mappedColor));
-  break;
-  case 5:
-  stroke (lerpColor(color(colors[contatore]), color(colors[contatore+1]), mappedColor));
-  break;
-  case 6:
-  stroke (lerpColor(color(colors[contatore]), color(colors[contatore+1]), mappedColor));
-  break;
-  case 7:
-  stroke (lerpColor(color(colors[contatore]), color(colors[contatore-7]), mappedColor));
-  break;
-}
+  switch (contatore) {
+    case 0:
+      stroke(lerpColor(color(colors[contatore]), color(colors[contatore + 1]), mappedColor));
+      break;
+    case 1:
+      stroke(lerpColor(color(colors[contatore]), color(colors[contatore + 1]), mappedColor));
+      break;
+    case 2:
+      stroke(lerpColor(color(colors[contatore]), color(colors[contatore + 1]), mappedColor));
+      break;
+    case 3:
+      stroke(lerpColor(color(colors[contatore]), color(colors[contatore + 1]), mappedColor));
+      break;
+    case 4:
+      stroke(lerpColor(color(colors[contatore]), color(colors[contatore + 1]), mappedColor));
+      break;
+    case 5:
+      stroke(lerpColor(color(colors[contatore]), color(colors[contatore + 1]), mappedColor));
+      break;
+    case 6:
+      stroke(lerpColor(color(colors[contatore]), color(colors[contatore + 1]), mappedColor));
+      break;
+    case 7:
+      stroke(lerpColor(color(colors[contatore]), color(colors[contatore - 7]), mappedColor));
+      break;
+  }
   beginShape();
 
-console.log(contatore);
-  if(colorProgress == giroCompleto-1){
+  // console.log(contatore);
+  if (colorProgress == giroCompleto - 1) {
     contatore++;
   }
-  if (contatore == 8){
+  if (contatore == 8) {
     contatore = 0;
   }
-//console.log(contatore);
+  //console.log(contatore);
 
   // contatore quando color progress arriva a 360 aggiunge 1
   // quando contatore arriva a 7 lo faccio tornare = 0
@@ -82,24 +81,21 @@ console.log(contatore);
   // mappare colorProgress per farlo diventare da 0 a 1 per il color lerp
 
   //console.log(colorProgress);
-// for( var v = 0; v < 4; v += 0.1){
+  // for( var v = 0; v < 4; v += 0.1){
 
-    for (var i = 0; i < giroCompleto; i+=1) {
-      //stroke(800, t, 100)
-      strokeWeight(3);
-      var ang = map(i, 0, giroCompleto, 0, TWO_PI);
-      var rad = 900 * noise(i * 0.01 , t * 0.005);
-      var x = rad * cos(ang);
-      var y = rad * sin(ang);
-      vertex(x, y);
-    }
+  for (var i = 0; i < giroCompleto; i += 1) {
+    //stroke(800, t, 100)
+    strokeWeight(3);
+    var ang = map(i, 0, giroCompleto, 0, TWO_PI);
+    var rad = 400 * noise(i * 0.01, t * 0.005);
+    var x = rad * cos(ang);
+    var y = rad * sin(ang);
+    vertex(x, y);
+  }
 
   endShape(CLOSE);
 
   t += 0.2 * mappedVolume;
-
-
-
 }
 
 function windowResized() {
